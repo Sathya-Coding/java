95eead6059c0f6777ad5932a4c6f6266cc340c8f
Sathya <psathyapriya2002@gmail.com>
2025-06-25 13:19:51 +0530

    Add files via upload

---
+++ countsstringarray.java
@@ -0,0 +1,10 @@
+public class countsstringarray {
+    public static void main(String[]args){
+        String[]fruits={"Apple","Orange","Mango","Banana"};
+        int count=0;
+        for(int i=0;i<fruits.length;i++){
+            count++;
+        }
+        System.out.println("Count of fruits : "+count);
+    }
+}
