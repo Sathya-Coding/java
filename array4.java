3af060364d1cabb7532d1c9e2830ac8e568a2d0d
Sathya <psathyapriya2002@gmail.com>
2025-06-25 10:54:35 +0530

    Add files via upload

--- array4.java
+++ array4.java
@@ -1,11 +1,14 @@
+import java.io.*;
 public class array4 {
     public static void main(String[]args){
-        int arr[]= v1();
-        for (int i=0;i<arr.length;i++)
-            System.out.println(arr[i]+"");
-
-        public static int [] v1(){
-            return new int[]{1, 2, 3};
+        int arr[]= m1();
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(arr[i] + "");
         }
     }
+        public static int[] m1()
+        {
+            return new int[]{1, 2, 3};
+        }
+
 }
